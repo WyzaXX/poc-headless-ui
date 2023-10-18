f4589357f80549795eb28a4d8bea275c577241ef
daniel.ivanov <daniel@enetpulse.com>
2023-10-18 14:58:27 +0300

    refactor

--- src/component/component.controller.ts
+++ src/component/component.controller.ts
@@ -1,42 +1,87 @@
 import { Controller, Get, Res } from '@nestjs/common';
 import { Response } from 'express';
 import { AwsService } from 'src/aws.service';
+import { helloWorld } from './hello-world';
+
+const s3BucketName = 'headless-ui-poc';
 
 @Controller('component')
 export class ComponentController {
   constructor(private awsService: AwsService) {}
+  s3 = this.awsService.getS3Instance();
 
   @Get()
   async getComponent(@Res() res: Response) {
-    const s3 = this.awsService.getS3Instance();
-    const s3BucketName = 'headless-ui-poc';
-    const s3ObjectKey = 'headless-ui5';
+    const s3ObjectKey = 'headless-ui';
 
+    await this.getOtSetComponentToS3(
+      helloWorld(),
+      this.s3,
+      s3BucketName,
+      s3ObjectKey,
+      res,
+    );
+  }
+
+  private async getOtSetComponentToS3(
+    component,
+    s3,
+    s3BucketName: string,
+    s3ObjectKey: string,
+    res: Response<any, Record<string, any>>,
+  ) {
     try {
-      const s3Object = await s3
-        .getObject({ Bucket: s3BucketName, Key: s3ObjectKey })
-        .promise();
-      const componentBuffer = s3Object.Body as Buffer;
+      const componentBuffer = await this.getComponentFromS3(
+        s3,
+        s3BucketName,
+        s3ObjectKey,
+      );
 
       console.log('got object from s3');
 
       res.send(componentBuffer);
     } catch (error) {
-      const componentHtml = '<div>Hello, Headless UI Component!</div>';
+      const componentHtml = component;
       const componentBuffer = Buffer.from(componentHtml, 'utf-8');
 
       console.log('not found in s3');
 
-      await s3
-        .upload({
-          Bucket: s3BucketName,
-          Key: s3ObjectKey,
-          Body: componentBuffer,
-          ContentType: 'text/html',
-        })
-        .promise();
+      await this.storeComponentToS3(
+        s3,
+        s3BucketName,
+        s3ObjectKey,
+        componentBuffer,
+      );
 
       res.send(componentHtml);
     }
+  }
+
+  private async storeComponentToS3(
+    s3,
+    s3BucketName: string,
+    s3ObjectKey: string,
+    componentBuffer: Buffer,
+  ) {
+    await s3
+      .upload({
+        Bucket: s3BucketName,
+        Key: s3ObjectKey,
+        Body: componentBuffer,
+        ContentType: 'text/html',
+      })
+      .promise();
+  }
+
+  private async getComponentFromS3(
+    s3,
+    s3BucketName: string,
+    s3ObjectKey: string,
+  ) {
+    const s3Object = await s3
+      .getObject({ Bucket: s3BucketName, Key: s3ObjectKey })
+      .promise();
+    const componentBuffer = s3Object.Body as Buffer;
+    return componentBuffer;
   }
 }
